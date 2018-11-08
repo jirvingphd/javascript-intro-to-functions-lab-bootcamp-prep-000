74636034797f4bd7990136a43666682192c5a6c3
James M. Irving <james.irving.phd@outlook.com>
2018-11-08 22:43:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-if string
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
+if {
+  string.toLowerCase() === string
+return("I can\'t hear you!")
+}
